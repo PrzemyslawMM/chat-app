16633c8f8c0ecf372aa7f58fec5f16adaa688bc2
Przemeks200iq <vertezelopl@gmail.com>
2022-08-23 21:38:34 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <>
+      <div>Test</div>
+      <a href="https://firebase.com" rel="noreferrer" target="_blank">
+        firebase
+      </a>
+    </>
+  );
 };
 
 export default App;
